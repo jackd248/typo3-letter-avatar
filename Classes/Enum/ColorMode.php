8d718d20152614de2ebdb8d3cfe8263d3c7032a7
Konrad Michalik <km@move-elevator.de>
2025-05-14 16:39:12 +0200

    feat: add ColorMode and ImageDriver enums for avatar configuration

---
+++ Classes/Enum/ColorMode.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KonradMichalik\Typo3LetterAvatar\Enum;
+
+enum ColorMode: string {
+    case STRINGIFY = 'stringify';
+    case RANDOM = 'random';
+}
---
+++ Classes/Enum/ImageDriver.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KonradMichalik\Typo3LetterAvatar\Enum;
+
+enum ImageDriver: string {
+    case IMAGICK = 'imagick';
+    case GMAGICK = 'gmagick';
+}
