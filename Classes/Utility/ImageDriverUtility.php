7f23c80ae1cf84f3e4b14d32288d4578898384b7
Konrad Michalik <konrad.michalik@gmx.de>
2025-05-16 16:19:00 +0200

    feat: add GdAvatar class for avatar generation

--- Classes/Utility/ImageDriverUtility.php
+++ Classes/Utility/ImageDriverUtility.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace KonradMichalik\Typo3LetterAvatar\Utility;
 
 use KonradMichalik\Typo3LetterAvatar\Enum\ImageDriver;
+use KonradMichalik\Typo3LetterAvatar\Image\Driver\GdAvatar;
 use KonradMichalik\Typo3LetterAvatar\Image\Driver\ImagickAvatar;
 use KonradMichalik\Typo3LetterAvatar\Image\LetterAvatarInterface;
+use TYPO3\CMS\Core\Utility\Exception\NotImplementedMethodException;
 
 class ImageDriverUtility
 {
@@ -16,6 +18,10 @@ class ImageDriverUtility
             case ImageDriver::IMAGICK:
             default:
                 return new ImagickAvatar(...$args);
+            case ImageDriver::GD:
+                return new GdAvatar(...$args);
+            case ImageDriver::GMAGICK:
+                throw new NotImplementedMethodException();
         }
     }
 }
